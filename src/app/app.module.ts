a0eeb459358075262f653f3708aa8def734988fb
nina.ruizandres@gmail.com <Atento02>
2020-03-28 19:55:47 +0100

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RoomPageComponent } from './room-page/room-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: MainPageComponent},
+  { path: 'room/:id', component: RoomPageComponent},
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +18,9 @@ import { MainPageComponent } from './main-page/main-page.component';
     MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
