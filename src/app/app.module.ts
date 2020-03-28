203f5601844d2fb03096e9cc960b8fa7190e0996
nina.ruizandres@gmail.com <Atento02>
2020-03-28 19:48:04 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoomPageComponent } from './room-page/room-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
